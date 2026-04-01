e1169751b0f1729b96d8c1e92bdf39b420ebafe7
Nicole Lourenço <nicolelourenco38@gmail.com>
2026-03-31 22:33:40 -0300

    testando

--- src/main/java/com/projeto/agendador_horarios/controller/AgendamentoController.java
+++ src/main/java/com/projeto/agendador_horarios/controller/AgendamentoController.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@RestController
+@RestController("/agendamentos")
 @RequiredArgsConstructor
 public class AgendamentoController {
     private final AgendamentoServices agendamentoServices;
